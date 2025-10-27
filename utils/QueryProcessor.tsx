386f3a9f4b730d96a9ce256a63eaa1cc1b3fed73
Shaikha Al-Emadi <shaikhaalemadi1708@gmail.com>
2025-10-27 08:52:11 +0000

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Shaikha";
+    return "S";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
